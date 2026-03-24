52e12e909c1911b5ce91ce1855866d03ea76562b
roalves1234 <roalves1234@hotmail.com>
2026-03-24 17:25:20 -0300

    Token errado

--- execution/uzapi.py
+++ execution/uzapi.py
@@ -3,4 +3,4 @@ class Uzapi:
     Classe de constantes para a integração com a API da Uzapi.
     """
     URL = "https://roalves1234.uazapi.com/send/text"
-    TOKEN = "7d74751e-ea7d-4375-95a0-82ad1b069b72"
+    TOKEN = "7d74751e-ea7d-4375-95a0-82ad1b069b72__"
