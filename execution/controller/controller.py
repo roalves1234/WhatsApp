9a47b89e286d7ff09a190440782fdac252ee79b9
roalves1234 <roalves1234@hotmail.com>
2026-04-04 17:46:00 -0300

    Tipos de interação

--- execution/controller/controller.py
+++ execution/controller/controller.py
@@ -1,8 +1,7 @@
 import textwrap
-from typing import Any
 
 from execution.controller.agente import Agente
-from execution.controller.classes import InteracaoUser
+from execution.controller.classes import InteracaoUser, InteracaoAssistant
 from execution.controller.home import Home
 from execution.controller.uzapi import Uzapi
 from execution.controller.version import Version
@@ -16,29 +15,28 @@ class Controller:
         return Home.get()
 
     @staticmethod
-    async def enviar_resposta(numero: str, texto: str) -> dict[str, Any]:
+    async def enviar_resposta_assistant(numero: str, texto_entrada: str) -> InteracaoAssistant:
         """
         Orquestra o fluxo completo de resposta inteligente:
-        1. Cria interação do usuário
-        2. Obtém a resposta da LLM via classe Agente (já instanciada no Controller)
-        3. Envia a resposta ao remetente via Uzapi.enviar_texto
+        1. Obtém a resposta da LLM via classe Agente (já instanciada no Controller)
+        2. Envia a resposta ao remetente via Uzapi.enviar_texto
         """
-        # Registra a interação do usuário
-        _interacao_user = InteracaoUser(mensagem=texto)
 
         # Consulta a LLM com o texto recebido pelo usuário usando a instância única
-        resposta_ia = Controller._agente.obter_resposta(texto)
+        interacao_assistant = Controller._agente.obter_resposta(texto_entrada)
 
         # Resposta da IA, incluindo o tempo de resposta e versão:
         texto_resposta = textwrap.dedent(f"""
-                                        {resposta_ia.mensagem}
-                                        🧠 {resposta_ia.conteudo.raciocinio}
-                                        ⏱ {resposta_ia.duracao}
+                                        {interacao_assistant.mensagem}
+                                        🧠 {interacao_assistant.conteudo.raciocinio}
+                                        ⏱ {interacao_assistant.duracao}
                                         🏷️ v{Version().get()}
                                         """).strip()
 
         # Simula digitação proporcional ao tamanho da resposta (mín. 2s, máx. 8s)
         await Uzapi.enviar_digitando(numero, texto_resposta)
 
-        # Retorno
-        return await Uzapi.enviar_texto(numero, texto_resposta)
+        # Envia a resposta para o WhatsApp
+        await Uzapi.enviar_texto(numero, texto_resposta)
+
+        return interacao_assistant
