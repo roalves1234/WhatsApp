d0bd669f1b9472362d7dabe250c3acc4101f10ca
roalves1234 <roalves1234@hotmail.com>
2026-04-04 15:10:26 -0300

    Resposta JSON

--- execution/controller/controller.py
+++ execution/controller/controller.py
@@ -1,3 +1,4 @@
+import textwrap
 from typing import Any
 
 from execution.controller.agente import Agente
@@ -27,9 +28,16 @@ class Controller:
         # Consulta a LLM com o texto recebido pelo usuário usando a instância única
         resposta_ia = Controller._agente.obter_resposta(texto)
 
-        # Simula digitação proporcional ao tamanho da resposta (mín. 2s, máx. 8s)
-        await Controller.enviar_digitando(numero, resposta_ia.content.resposta)
+        # Resposta da IA, incluindo o tempo de resposta e versão:
+        texto_resposta = textwrap.dedent(f"""
+                                        {resposta_ia.content.resposta}
+                                        🧠 {resposta_ia.content.raciocinio}
+                                        ⏱ {resposta_ia.time}                                        
+                                        🏷️ v{Version().get()}
+                                        """).strip()
 
-        # Encaminha a resposta da IA para o número de origem, incluindo o tempo de resposta
-        texto_resposta = f"{resposta_ia.content}\n⏱ {resposta_ia.time}\n🏷️ v{Version().get()}"
+        # Simula digitação proporcional ao tamanho da resposta (mín. 2s, máx. 8s)
+        await Controller.enviar_digitando(numero, texto_resposta)
+
+        # Retorno
         return await Uzapi.enviar_texto(numero, texto_resposta)
